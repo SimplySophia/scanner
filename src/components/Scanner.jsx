019ad1a23cdbaeeab2daf9eb3d6887ab863fa1d6
sophia vincent <sophire33@gmail.com>
2024-08-14 09:23:24 -0700

    integrated qr scanner module

---
+++ src/components/Scanner.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+import '../App.css';
+
+
+
+
+function Scanner() {
+  //const [count, setCount] = useState(0)
+
+  const [scanResult, setScanResult] = useState(null);
+
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner("reader", {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
+
+    scanner.render(success, error);
+
+    function success(result) {
+      scanner.clear();
+      setScanResult(result);
+    }
+
+    function error(err) {
+      console.warn(err);
+    }
+  }, []);
+
+  return (
+    <div className="Scanner">
+      {scanResult ? (
+        <div>
+          Success: <a href={scanResult} target="_blank" rel="noopener noreferrer">{scanResult}</a>
+        </div>
+      ) : (
+        <div id="reader"></div>
+      )}
+    </div>
+  );
+}
+
+export default Scanner;
